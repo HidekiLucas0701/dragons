3c9e4fa8ef90ceec03f2ce29d85c06421c7f7aaf
Lucas Hideki <hideki.lucas0701@gmail.com>
2026-03-10 19:37:05 -0300

    feat(wip): creating dragon entity with getters and setters from lombok

---
+++ src/main/java/br/com/fiap/dragons/models/Dragon.java
@@ -0,0 +1,24 @@
+package br.com.fiap.dragons.models;
+
+import lombok.Data;
+
+@Data
+public class Dragon {
+
+    private Integer id;
+    private String nome;
+    private String cor;
+    private Integer poderDeFogo;
+    private Long peso;
+    private Long altura;
+    private Boolean possuiMontador;
+
+    public Dragon(String nome, String cor, Integer poderDeFogo, Long peso, Long altura, Boolean possuiMontador) {
+        this.nome = nome;
+        this.cor = cor;
+        this.poderDeFogo = poderDeFogo;
+        this.peso = peso;
+        this.altura = altura;
+        this.possuiMontador = possuiMontador;
+    }
+}
